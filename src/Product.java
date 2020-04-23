c225d538a440ec97acf0b02961ef698167667855
Rezaafg <rezahosseiniafg@gmail.com>
2020-04-23 08:36:43 +0200

    Add Product

---
+++ src/Product.java
@@ -0,0 +1,34 @@
+
+public class Product {
+
+	    private String name = "Coca Cola2";
+	    private float price1 =10;
+	    private int id;
+
+	    public Product(String name, float price ,int id)
+	    {
+	     
+	
+	    }
+	   private float price = 10;
+	    public Product(String name)
+	    {
+	        setName(name);
+	  
+	    }
+
+	    public void setName(String name)
+	    {
+	        this.name=name;
+	    }
+
+
+	      public float getPrice()
+	    {
+	      return price1;
+	    }
+	     public int getId()
+	    {
+	      return id;
+	    }
+}
